fe96d594a84976774cdf94baa1aa109debd63fba
RedRope <ajay.lecluse@lilo.org>
2019-11-14 21:31:08 +0100

    functions reset and newGrid added

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ function buildGrid(number){
     myGrid.setAttribute("style", "grid-template-columns : repeat("+ number + ",1fr); grid-template-rows : repeat("+ number + ", 1fr)")
     for (let i = 1; i <= squareNumber; i++){
         const newCell = document.createElement("div");
-       
-        newCell.classList.add("cell");
+        newCell.setAttribute("class", "cell");
         newCell.setAttribute("id", i);
         newCell.setAttribute("onmouseover", "blackBg()");
         myGrid.appendChild(newCell);
@@ -19,10 +18,25 @@ buildGrid(20);
 gridCells = document.querySelectorAll(".cell");
 
 
+
+
 function blackBg() {
     document.getElementById(event.target.id).setAttribute('style', 'background-color: black');
   }
 
 
+function reset() {
+    gridCells = document.querySelectorAll(".cell");
+    let number =gridCells.length;
+    for (let i = 1; i <= number; i++){
+        let cell = document.getElementById(i);
+        myGrid.removeChild(cell);
+    }
+}
 
-
+function newGrid(){
+    let num = prompt("Size of new grid :");
+    reset()
+    buildGrid(num);
+    console.log("build")
+}
