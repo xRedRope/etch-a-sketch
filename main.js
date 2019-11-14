ef2d72d9df07cc21c58b6e3d4b7dfc074892ca5c
RedRope <ajay.lecluse@lilo.org>
2019-11-14 19:05:25 +0100

    created a function blackBg and deleted wrong js

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
+
+let gridCells;
 const myGrid = document.querySelector(".grid");
 function buildGrid(number){
     let squareNumber = number * number;
     myGrid.setAttribute("style", "grid-template-columns : repeat("+ number + ",1fr); grid-template-rows : repeat("+ number + ", 1fr)")
     for (let i = 1; i <= squareNumber; i++){
-        const myGridDiv = document.createElement("div");
-        myGridDiv.textContent = i;
-        myGridDiv.classList.add("box");
-        myGridDiv.setAttribute("id", i);
-        myGrid.appendChild(myGridDiv);
+        const newCell = document.createElement("div");
+       
+        newCell.classList.add("cell");
+        newCell.setAttribute("id", i);
+        newCell.setAttribute("onmouseover", "blackBg()");
+        myGrid.appendChild(newCell);
     }
 }
 
+buildGrid(20);
 
-const myDiv = document.querySelectorAll(".box");
+gridCells = document.querySelectorAll(".cell");
 
-//const test = document.querySelector("#5");
-//test.addEventListener('onclick', () => {
-//    alert(test.id);
-//});
 
-myDiv[8].style.backgroundColor = "red";
-myDiv.forEach((div) => {
-    div.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = "blue";
-    });
-});    
-myDiv.addEventListener("mouseenter", function(e){
-    e.target.style.backgroundColor = "blue";
-});
+function blackBg() {
+    document.getElementById(event.target.id).setAttribute('style', 'background-color: black');
+  }
 
-const reset = document.querySelector(".reset");
 
-reset.onclick = () => alert("suh up");
+
+
